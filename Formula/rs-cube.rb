873e650c17e0733ac7d2759febdeee9a98bafc8b
doprz <52579214+doprz@users.noreply.github.com>
2023-04-04 21:57:11 -0500

    feat: add v1.0.2 homebrew formula

--- Formula/rs-cube.rb
+++ Formula/rs-cube.rb
@@ -1,8 +1,8 @@
 class RsCube < Formula
   desc "Blazingly fast 3D ASCII Rubik's Cube"
   homepage "https://github.com/doprz/rs-cube"
-  url "https://github.com/doprz/rs-cube/archive/v1.0.1.tar.gz"
-  sha256 "006d2e96e12a066ba1f4fd74a8dd07f0e4b75437836cc5cbf3900adc74de607c"
+  url "https://github.com/doprz/rs-cube/archive/v1.0.2.tar.gz"
+  sha256 "afc95208098776ee9d13b574928788f8c6d64381f033d467da03022d5ef20686"
   license any_of: ["MIT", "Apache-2.0"]
 
   depends_on "rust" => :build
